a89df63242c32c723476fd5c1ead23a23cf90028
sanevs <vavland@mail.ru>
2024-09-15 14:52:19 +0300

    Add new shape

--- GeometryTest/ShapesUnitTest.cs
+++ GeometryTest/ShapesUnitTest.cs
@@ -19,29 +19,40 @@ namespace GeometryTest
             Triangle triangle = new Triangle(3, 4, 5);
             Assert.AreEqual(6, triangle.CalcSquare());
         }
+
         [TestMethod]
         public void TestIsRectangularTriangle() 
         {
             Triangle triangle = new Triangle(3, 4, 5);
             Assert.IsTrue(triangle.IsRectangular());
         }
+
         [TestMethod]
         public void TestIsNotRectangularTriangle() 
         {
             Triangle triangle = new Triangle(3, 4, 7);
             Assert.IsFalse(triangle.IsRectangular());
         }
+
         [TestMethod]
         public void TestCalculationCircleAsShapeType() 
         {
             IShape circle = new Circle(3);
             Assert.AreEqual(28, circle.CalcSquare());
         }
+
         [TestMethod]
         public void TestCalculationTriangleAsShapeType() 
         {
             IShape triangle = new Triangle(13, 12, 5);
             Assert.AreEqual(30, triangle.CalcSquare());
+        }
+
+        [TestMethod]
+        public void TestEasyAddingNewShape()
+        {
+            IShape rectangle = new Rectangle(4, 5);
+            Assert.AreEqual(20, rectangle.CalcSquare());
         }
     }
 }
---
+++ ShapesApp/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesApp
+{
+    public class Rectangle : IShape
+    {
+        public int A { get; }
+        public int B { get; }
+
+        public Rectangle(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public int CalcSquare()
+        {
+            return A * B;
+        }
+    }
+}
